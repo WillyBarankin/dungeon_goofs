10b0d313db050e64e7a5677f6cf612ea3c80ad4d
WillyBarankin <rs.vetyshev@gmail.com>
2022-12-29 10:10:05 +0300

    Optimized drawing process.

--- practice.cpp
+++ practice.cpp
@@ -12,7 +12,7 @@ int getRandomNumber(int param_a, int param_b)
 	return(distrib(random));
 }
 
-int getSpaceLeft(int bs_ch, int box_pos_y, int box_pos_x, int box_height, int box_length) 
+int getSpaceLeft(WINDOW *win_nm, int bs_ch, int box_pos_y, int box_pos_x, int box_height, int box_length) 
 {
 	int ch_left = 0;
 	 
@@ -21,7 +21,7 @@ int getSpaceLeft(int bs_ch, int box_pos_y, int box_pos_x, int box_height, int bo
 		for (int k = 0; k < box_length; k++)
 		{
 			vector<int> ch_ar(box_length);
-			ch_ar[k] = (mvinch(box_pos_y+g, box_pos_x+k) & A_CHARTEXT);
+			ch_ar[k] = (mvwinch(win_nm, box_pos_y+g, box_pos_x+k) & A_CHARTEXT);
 
 			if (ch_ar[k] == bs_ch)
 			{
@@ -47,14 +47,14 @@ void launchGame(int row, int col)
 	int box_length = getRandomNumber(6, col-1);
 	int box_height = getRandomNumber(5, row-3);
 
-	WINDOW * g_winPtr = newwin(getRandomNumber(5, row), getRandomNumber(6, col), (row - box_height)/2, (col - box_length)/2);
-	int g_win_sy, g_win_my, g_win_sx, g_win_mx = 0;
-	getbegyx(g_winPtr, g_win_sy, g_win_sx);
+	WINDOW * g_winPtr = newwin(box_height, box_length, (row - box_height)/2, (col - box_length)/2);
+	int g_win_sy, g_win_my, g_win_sx, g_win_mx;
+	getparyx(g_winPtr, g_win_sy, g_win_sx);
 	getmaxyx(g_winPtr, g_win_my, g_win_mx);
+
 	box(g_winPtr, 1, 1);
 	wattron(g_winPtr, COLOR_PAIR(1));
 	wborder(g_winPtr, '#' ,'#' ,'#' ,'#' ,'#' ,'#' ,'#' ,'#');
-	wprintw(g_winPtr, "%d", mvwinch(g_winPtr, 0, 5) & A_CHARTEXT);
 	refresh();
 	wrefresh(g_winPtr);
 
@@ -62,22 +62,23 @@ void launchGame(int row, int col)
 
 	for (int i=0; i <= obsticles_n; i++)
 	{
-		attron(COLOR_PAIR(4));
-		mvhline(getRandomNumber(g_win_sy+1, g_win_my-1), getRandomNumber(g_win_sx+1, g_win_mx-4), 'X', getRandomNumber(1, 4));
-		mvvline(getRandomNumber(g_win_sy+1, g_win_my-3), getRandomNumber(g_win_sx+1, g_win_mx-1), 'X', getRandomNumber(1, 3));
-		mvvline(getRandomNumber(g_win_sy+1, g_win_my-1), getRandomNumber(g_win_sx+1, g_win_mx-1), 'f' | COLOR_PAIR(5), 1);
+		wattron(g_winPtr, COLOR_PAIR(4));
+		mvwhline(g_winPtr, getRandomNumber(g_win_sy+2, g_win_my-2), getRandomNumber(g_win_sx+2, g_win_mx-5), 'X', getRandomNumber(1, 4));
+		mvwvline(g_winPtr, getRandomNumber(g_win_sy+2, g_win_my-4), getRandomNumber(g_win_sx+2, g_win_mx-2), 'X', getRandomNumber(1, 3));
+		mvwvline(g_winPtr, getRandomNumber(g_win_sy+2, g_win_my-2), getRandomNumber(g_win_sx+2, g_win_mx-2), 'f' | COLOR_PAIR(5), 1);
+		wrefresh(g_winPtr);
 	}
 
 	int unit_count  = obsticles_n/2;
-	vector<int>	char_pos_xx(unit_count);
-	vector<int>	char_pos_yy(unit_count);
-	vector<int>	ch(unit_count);
+	vector<int> char_pos_xx(unit_count);
+	vector<int> char_pos_yy(unit_count);
+	vector<int> ch(unit_count);
 
 	for (int i = 0; i< unit_count; i++)
 	{
 		char_pos_yy[i] = getRandomNumber(g_win_sy+2, g_win_my-1);
 		char_pos_xx[i] = getRandomNumber(g_win_sx+2, g_win_mx-1);
-		ch[i] = (mvinch(char_pos_yy[i], char_pos_xx[i]) & A_CHARTEXT);
+		ch[i] = (mvwinch(g_winPtr, char_pos_yy[i], char_pos_xx[i]) & A_CHARTEXT);
 
 		while (ch[i] == 35 || ch[i] == 88 || ch[i] == 64 || ch[i] == 102) 
 		{
@@ -86,7 +87,7 @@ void launchGame(int row, int col)
 			ch[i] = (mvwinch(g_winPtr, char_pos_yy[i], char_pos_xx[i]) & A_CHARTEXT);
 		}
 
-		mvaddch(char_pos_yy[i], char_pos_xx[i], '@' | A_BOLD | COLOR_PAIR(3));
+		mvwaddch(g_winPtr, char_pos_yy[i], char_pos_xx[i], '@' | A_BOLD | COLOR_PAIR(3));
 	}
 
 	while (1)
@@ -106,21 +107,21 @@ void launchGame(int row, int col)
 				ch[j] = (mvwinch(g_winPtr, char_pos_yy[j], char_pos_xx[j]) & A_CHARTEXT);
 			}
 
-			mvaddch(char_pos_yy[j], char_pos_xx[j], '@' | A_BOLD | COLOR_PAIR(3));
+			mvwaddch(g_winPtr, char_pos_yy[j], char_pos_xx[j], '@' | A_BOLD | COLOR_PAIR(3));
 
-			mvaddch(old_pos_y, old_pos_x, '.' | COLOR_PAIR(2));
+			mvwaddch(g_winPtr, old_pos_y, old_pos_x, '.' | COLOR_PAIR(2));
 		}
 
 		timeout(150);
 
-		int ch_left = getSpaceLeft(32, g_win_sy, g_win_sx, box_height, box_length);
+		int ch_left = getSpaceLeft(g_winPtr, 32, g_win_sy, g_win_sx, g_win_my, g_win_mx);
 		move(row-1, 1);
 		printw("Space left: %d", ch_left);
 		clrtoeol();
 		mvprintw(row-1, col-20, "Press 'q' to stop.", ch_left);
 		clrtoeol();
-
-		refresh();
+ 
+		wrefresh(g_winPtr);
 
 		char input = getch();
 		if (input == 'q' || ch_left == 0)
@@ -153,7 +154,6 @@ int showMenu()
 	m_start_y = y_row/2-m_height/2;
 
 	WINDOW * m_winPtr = newwin(m_height, m_width, m_start_y, m_start_x);
-	refresh();
 
 	box(m_winPtr, 0, 0);
 	mvwprintw(m_winPtr, 1, 1, "Main menu:");
@@ -161,6 +161,7 @@ int showMenu()
 	mvwprintw(m_winPtr, 3, 1, "2)Options");
 	mvwprintw(m_winPtr, 4, 1, "3)Quit");
 	move(y_row-1, x_col-1);
+	refresh();
 	wrefresh(m_winPtr);
 
 	input = getch();
